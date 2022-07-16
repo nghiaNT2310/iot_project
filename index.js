847120a1e46bc64674853f78b5e274dc4cf296e2
nghiaNT2310 <nghia.nt194341@sis.hust.edu.vn>
2022-07-16 11:08:55 +0700

    finish

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ async function connect() {
 app.use( function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
    
     next()
   });
